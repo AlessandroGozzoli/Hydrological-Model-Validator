1ad66f0a182b826c8f29ae49e39adbc58700d9af
alessandro gozzoli <alessandro.gozzoli4@studio.unibo.it>
2025-05-28 17:35:38 +0200

    Refractored test case, now supports interpolator matlab engine

--- Test_cases/Data_reader_setupper.py
+++ Test_cases/Data_reader_setupper.py
@@ -34,6 +34,7 @@ print('*'*45)
 import os
 from pathlib import Path
 import sys
+import matlab.engine
 
 # To create necessary folders
 from datetime import datetime
@@ -63,14 +64,6 @@ print(f"The datasets are located in the folder {BaseDIR} !")
 DSAT = Path(BaseDIR, "SATELLITE/")
 sys.path.append(str(DSAT))  # Add the folder to the system path
 print(f"Satellite data located at {DSAT} !")
-# SST
-DSST_sat = Path(DSAT, "SST/")
-sys.path.append(str(DSST_sat))  # Add the folder to the system path
-print(f"Satellite SST data located at {DSST_sat}")
-# SCHL
-DCHL_sat = Path(DSAT, "SCHL/")
-sys.path.append(str(DCHL_sat))  # Add the folder to the system path
-print(f"Satellite CHL data locate at {DCHL_sat}")
 
 # ----- DIRECTORIES FOR MODEL DATA
 Dmod = Path(BaseDIR, "MODEL")
@@ -85,19 +78,16 @@ print('*'*45)
 ##                                                                           ##
 ###############################################################################
 
-print("Retrieving the time utility necessary functions...")
-from Hydrological_model_validator.Processing.time_utils import true_time_series_length
-print("\033[92m✅ Time utility functions retrieved!\033[0m")
-
 print("Retrieving the File I/O necessary functions...")
-from Hydrological_model_validator.Processing.file_io import mask_reader
+from Hydrological_model_validator.Processing.file_io import (mask_reader,
+                                                             call_interpolator)
 print("\033[92m✅ File I/O functions retrieved!\033[0m")
 
 print("Retrieving the necessary functions to read the satellite and model data...")
-from Hydrological_model_validator.Processing.SAT_data_reader import sat_chldata, read_sst_satellite_data
+from Hydrological_model_validator.Processing.SAT_data_reader import (sat_data_loader)
 
 # Reads the model datasets
-from Hydrological_model_validator.Processing.MOD_data_reader import read_model_chl_data, read_model_sst, Bavg_sst
+from Hydrological_model_validator.Processing.MOD_data_reader import read_model_data
 print("\033[92m✅ Functions to read the model and satellite data retrieved!\033[0m")
 
 print("Retrieving the functions necessary to identify missing data...")
@@ -109,28 +99,10 @@ from Hydrological_model_validator.Processing.Missing_data import (
 print("\033[92m✅ Data functions for the identification of missing data retrieved!\033[0m")
 
 print("Retrieving the data saving functions...")
-from Hydrological_model_validator.Processing.Data_saver import save_satellite_CHL_data, save_satellite_SST_data, save_model_CHL_data, save_model_SST_data, save_SST_Bavg
+from Hydrological_model_validator.Processing.Data_saver import (save_satellite_data,
+                                                                save_model_data)
 print("\033[92m✅ Data saving functions retrieved!\033[0m")
-
-print("Retrieving some costants...")
-print("These will be deprecated in a future update!")
-from Hydrological_model_validator.Processing.Costants import (
-                      DinY,
-                      SinD,
-                      nf,
-                      chlfstart,
-                      chlfend,
-                      startingyear,
-                      startingmonth,
-                      startingday,
-                      LE,
-                      LS,
-                      total_SZTtmp,
-                      Ybeg,
-                      Tspan,
-                      ysec
-                      )
-print("\033[92m✅ Costants retrieved!\033[0m")
+print('*'*45)
 
 ###############################################################################
 ##                                                                           ##
@@ -138,114 +110,85 @@ print("\033[92m✅ Costants retrieved!\033[0m")
 ##                                                                           ##
 ###############################################################################
 
-###############################################################################
-##                                                                           ##
-##                         SATELLITE - CHLOROPHYLLE                          ##
-##                                                                           ##
-###############################################################################
+print("Due to the size of the datasets used the processing of both the")
+print("Chlorphyll and Sea Surface Temperature dataset cannot be performed")
+print("in the same run")
 
-print("Beginning the data cleanup...")
-
-print("Which level of satellite data would you like to handle?")
-print("\033[91mCareful, the datasets might be to big to be handled by the code\033[0m")
-print("\033[91mHence why it is suggested to make a run of the script for each data level\033[0m")
-# ASK THE USER FOR THE LEVEL OF CHL DATA TO BE READ
-# The code cannot run both toghether due to excessive RAM requirements (test case is 22+2Gb)
-# Data divided in level 3 or 4 data
 while True:
-    chldlev = input("Enter CHL satellite data level (l3 or l4): ").lower()
-    if chldlev in ['l3', 'l4']:
+    varname = input("What kind of dataset would you like to read and process? (CHL or SST): ").lower()
+    if varname in ['chl', 'sst']:
         break
-    print("\033[91m⚠️ Invalid input. Please enter either 'l3' or 'l4' ⚠️\033[0m")
+    print("\033[91m⚠️ Invalid input. Please enter either 'CHL' or 'SST' ⚠️\033[0m")
 
-print(f"\033[92m✅ Satellite CHL data level set to: {chldlev}\033[0m")
-print('-'*45)
+print(f"\033[92m✅ We are going to work on the {varname.upper()} dataset\033[0m")
+print('-' * 45)
 
-# Definition of some variables needed to compose the file output names
-ffrag1 = "NADR-CHL-SAT-CMEMS-"
-ffrag2 = "daily"
+print("The computation for both data levels cannot be handled simultaneously.")
+print("The possible satellite dataset levels are:")
+print(" - L3 (Super-collated): A single gridded product without interpolation.")
+print(" - L4 : A gap-free dataset already interpolated.")
 
-Schl2r = 'CHL'
+while True:
+    data_level = input("Enter the satellite data level (L3 or L4): ").lower()
+    if data_level in ['l3', 'l4']:
+        break
+    print("\033[91m⚠️ Invalid input. Please enter either 'L3' or 'L4' ⚠️\033[0m")
 
-# COMPUTE THE NUMBER OF DAYS IN THE DATASET
-Truedays = true_time_series_length(nf, chlfstart, chlfend, DinY)
+print(f"\033[92m✅ Satellite data level set to: {data_level.upper()}\033[0m")
+print('-' * 45)
 
-# Assert to check if the calculated number of days is 3652
-assert Truedays == 3653, f"\033[91m⚠️ Error: Expected 3653 days, but got {Truedays}⚠️\033[0m"
+###############################################################################
+##                                                                           ##
+##                                SATELLITE                                  ##
+##                                                                           ##
+###############################################################################
 
-print("The number of days in the dataset accounting for leap years is:", Truedays)
-print('-'*45)
+if varname == 'sst':
+    Data_sat = Path(DSAT, "SST/")
+    sys.path.append(str(Data_sat))  # Add the folder to the system path
+    print(f"Satellite SST data located at {Data_sat}")
+elif varname == 'chl':
+    Data_sat = Path(DSAT, "SCHL/")
+    sys.path.append(str(Data_sat))  # Add the folder to the system path
+    print(f"Satellite CHL data locate at {Data_sat}")
 
-# READ THE CHL SATELLITE DATA
 # Call the function to read the data, merge it and check for SLON, SLAT shapes
-T_orig, Schl_orig, Slon, Slat, Schlpath = sat_chldata(
-    total_SZTtmp, LE, LS, nf, ffrag1, chlfstart, chlfend, chldlev, ffrag2, DCHL_sat, Schl2r
-)
+T_orig, SatData_orig, Sat_lon, Sat_lat = sat_data_loader(data_level, Data_sat, varname)
 
-# Run the missing days checker
-Ttrue, Schl_complete=check_missing_days(T_orig, Schl_orig, Truedays, SinD, startingyear, startingmonth, startingday)
+# Run the functions to check for missing days/values/fields
+Ttrue, SatData_complete=check_missing_days(T_orig, SatData_orig)
 
 # Run the missing observation checker
-satnan = find_missing_observations(Schl_complete, Truedays)
+satnan = find_missing_observations(SatData_complete)
 
 # Run the empty field checker
-Schl_complete = eliminate_empty_fields(Schl_complete, Truedays)
+Schl_complete = eliminate_empty_fields(SatData_complete)
 
-save = input("Do you want to save the Satellite CHL data? (yes/no): ").strip().lower()
+save = input("Do you want to save the Satellite data? (yes/no): ").strip().lower()
 print('-' * 45)
 
 if save in ["yes", "y"]:
     # Create a timestamped folder for this run
     timestamp = datetime.now().strftime("run_%Y-%m-%d")
-    output_path = os.path.join(BaseDIR, "OUTPUT", "SATELLITE", chldlev, timestamp)
+    output_path = os.path.join(BaseDIR, "OUTPUT", "CLEANING", varname, timestamp)
+    print(f"The data is being saved in the {output_path} folder for easier access for the interpolator")
     os.makedirs(output_path, exist_ok=True)
 
     print(f"Saving files in folder: {output_path}")
     print('-' * 45)
 
     # Call the save function and pass the new path
-    save_satellite_CHL_data(output_path, Slon, Slat, Schl_complete)
-
-else:
-    print("You chose not to save the data")
-    print('*' * 45)
-
-###############################################################################
-##                                                                           ##
-##                    SATELLITE - SEA SURFACE TEMPERATURE                    ##
-##                                                                           ##
-###############################################################################
-
-print("Starting to read the satellite SST data...")
-Sat_sst = read_sst_satellite_data(DSST_sat, Truedays)
-print("Satellite SST retrieval completed!")
-print("*"*45)
-
-save = input("Do you want to save the Satellite SST data? (yes/no): ").strip().lower()
-print('-' * 45)
-
-if save in ["yes", "y"]:
-    # Create a timestamped folder for this run
-    timestamp = datetime.now().strftime("run_%Y-%m-%d")
-    output_path = os.path.join(BaseDIR, "OUTPUT", "SATELLITE", "SST", timestamp)
-    os.makedirs(output_path, exist_ok=True)
-
-    print(f"Saving files in folder: {output_path}")
-    print('-' * 45)
-
-    # Call the save function and pass the new path
-    save_satellite_SST_data(output_path, Sat_sst)
+    save_satellite_data(output_path, Sat_lon, Sat_lat, SatData_complete)
 
 else:
     print("You chose not to save the data")
     print('*' * 45)
     
-print("Satellite data completed!")
-print("*"*45)
+print("Please do not move the newly saved files!")
 
 ###############################################################################
 ##                                                                           ##
-##                           MODEL - CHLOROPHYLLE                            ##
+##                                  MODEL                                    ##
 ##                                                                           ##
 ###############################################################################
 
@@ -302,8 +245,8 @@ else:
 
 # READING THE DATA
 print('*'*45)
-print("Reading the CHL data...")
-Mchl_complete = read_model_chl_data(Dmod, Ybeg, Tspan, Truedays, DinY, Mfsm)
+print("Reading the data...")
+ModData_complete = read_model_data(Dmod, Mfsm, varname)
 
 save = input("Do you want to save the Model CHL data? (yes/no): ").strip().lower()
 print('-' * 45)
@@ -311,93 +254,49 @@ print('-' * 45)
 if save in ["yes", "y"]:
     # Create a timestamped folder for this run
     timestamp = datetime.now().strftime("run_%Y-%m-%d")
-    output_path = os.path.join(BaseDIR, "OUTPUT", "MODEL", "CHL", timestamp)
+    output_path = os.path.join(BaseDIR, "OUTPUT", "CLEANING", varname, timestamp)
     os.makedirs(output_path, exist_ok=True)
 
     print(f"Saving files in folder: {output_path}")
     print('-' * 45)
 
     # Call the save function and pass the new path
-    save_model_CHL_data(output_path, Mchl_complete)
+    save_model_data(output_path, ModData_complete)
 
 else:
     print("You chose not to save the data")
     print('*' * 45)
 
+print("Please do not move the newly saved files!")
+
 ###############################################################################
 ##                                                                           ##
-##                      MODEL - SEA SURFACE TEMPERATURE                      ##
+##                              INTERPOLATOR                                 ##
 ##                                                                           ##
 ###############################################################################
 
-print("Starting to read the MODEL SST data...")
-Msst_complete = read_model_sst(Dmod, ysec, Mfsm)
-print("\033[92m✅ The full Model SST data has been retrieved!\033[0m")
-print('*'*45)
-
-save = input("Do you want to save the Model SST data? (yes/no): ").strip().lower()
+interpolate = input("Dou you wish to call the interpolator to process the data? (yes/no): ").strip().lower()
 print('-' * 45)
 
-if save in ["yes", "y"]:
-    # Create a timestamped folder for this run
+if interpolate in ["yes", "y"]:
+
     timestamp = datetime.now().strftime("run_%Y-%m-%d")
-    output_path = os.path.join(BaseDIR, "OUTPUT", "MODEL", "SST", timestamp)
-    os.makedirs(output_path, exist_ok=True)
+    interp_output_path = os.path.join(BaseDIR, "OUTPUT", "INTERPOLATED", varname, data_level)
+    os.makedirs(interp_output_path, exist_ok=True)
 
-    print(f"Saving files in folder: {output_path}")
-    print('-' * 45)
+    # Reuse the output path of the savng functions
+    input_path = output_path
 
-    # Call the save function and pass the new path
-    save_model_SST_data(output_path, Msst_complete)
+    Mask_path=os.path.join(BaseDIR)
 
-else:
-    print("You chose not to save the data")
-    print('*' * 45)
-    
-###############################################################################
-##                                                                           ##
-##                              BASIN AVERAGES                               ##
-##                                                                           ##
-###############################################################################
+    print("Calling the MatLab interpolator function...")
+    call_interpolator(varname, data_level, input_dir=input_path, output_dir=interp_output_path, mask_file=Mask_path)
 
-Bavg = input("Do you wish to compute the Basin Averages? (yes/no): ").strip().lower()
-print('-'*45)
-
-if Bavg in ["yes", "y"]:
-    # WARNING ABOUT THE CHL DATA
-    print("\033[91m⚠️ WARNING ABOUT THE CHLOROPHYLLE DATA ⚠️")
-    print("\033[91m Due to the high concentration of Nan fields and other ")
-    print(" missing data in the CHL fields (especially the satellite ones) ")
-    print(" the basin avegares computations can be done only after the ")
-    print(" run of the interpolator.m script to ensure that the missing ")
-    print(" fields are appropriatelly reconstructed. \033[0m")
-    
-    # Computing the SST Basin Averages
-    print("Creating the Basin Average timeseries of model and satellite SST data...")
-    BASSTmod, BASSTsat = Bavg_sst(Tspan, ysec, Dmod, Sat_sst, Mfsm)
-    print("\033[92m✅ Basin Average Daily Mean Timeseries computed! \033[0m")
-    
-    save = input("Do you want to save the basin average SST data? (yes/no): ").strip().lower()
-
-    if save in ["yes", "y"]:
-        
-        # Create a timestamped folder for this run
-        timestamp = datetime.now().strftime("run_%Y-%m-%d")
-        output_path = os.path.join(BaseDIR, "OUTPUT", "BASIN_AVERAGES", "SST", timestamp)
-        os.makedirs(output_path, exist_ok=True)
-
-        print(f"Saving files in folder: {output_path}")
-        print('-' * 45)
-
-        # Call the save function and pass the new path
-        save_SST_Bavg(output_path, BASSTmod, BASSTsat)
-        
-    print("Please proceed with either the interpolation of the missing satellite data")
-    print("or with the analysis and validation of the datasets")
-    print('*'*45)
+    print("The data has been succesfully interpolated and the Basin Averages have been computed!")
+    print("Please retrieve the new datasets from the folder!")
     
 else:
-    print("You chose not to compute the Basin Averages data")
-    print("Please proceed with either the interpolation of the missing satellite data")
-    print("or with the analysis and validation of the datasets")
-    print('*'*45)
+    print("You chose not to interpolate the data")
+   
+print("You may proceed with the analysis scripts!")
+print('*'*45)
