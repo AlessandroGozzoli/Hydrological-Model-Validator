ad4c67701d68d08add2a0165ea8092e126784c2f
alessandro gozzoli <alessandro.gozzoli4@studio.unibo.it>
2025-05-28 17:36:05 +0200

    Updated setup.py to include the matlab scripts

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='Hydrological_model_validator',
     version='4.0.0',
     packages=find_packages(),
+    include_package_data=True,  # Include package data files specified in package_data or MANIFEST.in
+    package_data={
+        # Assuming 'Hydrological_model_validator' is your main package directory name
+        'Hydrological_model_validator': ['Processing/*.m'],
+    },
     install_requires=[
         'numpy',
         'pandas',
