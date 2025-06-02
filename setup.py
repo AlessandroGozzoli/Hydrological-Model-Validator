f383dba980b20fecff54177fec360bafded8df2e
alessandro gozzoli <alessandro.gozzoli4@studio.unibo.it>
2025-06-02 14:12:41 +0200

    Updated setup ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Hydrological_model_validator',
-    version='4.3.1',
+    version='4.3.2',
     packages=find_packages(),
     include_package_data=True,  # Include package data files specified in package_data or MANIFEST.in
     package_data={
