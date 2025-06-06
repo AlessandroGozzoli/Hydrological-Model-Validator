ed0ce49ec1aa35cee1d850e025481e14e41ae5e3
alessandro gozzoli <alessandro.gozzoli4@studio.unibo.it>
2025-06-06 10:10:06 +0200

    Expanded setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
+
+__author__ = ['Alessandro Gozzoli']
+__email__ = ['alessandro.gozzoli4@studio.unibo.it']
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='Hydrological_model_validator',
     version='4.7.1',
+    description='Tools for the analysis and validation of Bio-Geo-Hydrological simulations and other climatological data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alessandro Gozzoli',
+    author_email='alessandro.gozzoli4@studio.unibo.it',
+    url='https://github.com/AlessandroGozzoli/Hydrological-Model-Validator',
+    download_url='https://github.com/AlessandroGozzoli/Hydrological-Model-Validator',
     packages=find_packages(),
-    include_package_data=True,  # Include package data files specified in package_data or MANIFEST.in
+    include_package_data=True,
     package_data={
         'Hydrological_model_validator': ['Processing/*.m'],
     },
@@ -27,4 +43,21 @@ setup(
         'dask'
     ],
     python_requires='>=3.7',
+    platforms='any',
+    classifiers=[
+      'Natural Language :: English',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: POSIX',
+      'Operating System :: POSIX :: Linux',
+      'Operating System :: Microsoft :: Windows',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
+      'Programming Language :: Python :: 3.11',
+      'Programming Language :: Python :: Implementation :: CPython',
+      'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    entry_points={}
 )
